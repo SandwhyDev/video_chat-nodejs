63e2f586944c73756dc3f2631a1f3d79ed29306f
SandwhyDev <sandwhydev@gmail.com>
2023-02-20 14:11:17 +0700

    update

--- public/script.js
+++ public/script.js
@@ -1,4 +1,19 @@
 const socket = io("/");
+function uuid() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+const userStatus = {
+  microphone: false,
+  mute: false,
+  username: user,
+  online: false,
+};
+
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 const showChat = document.querySelector("#showChat");
@@ -19,7 +34,14 @@ showChat.addEventListener("click", () => {
   document.querySelector(".header__back").style.display = "block";
 });
 
-const user = prompt("Enter your name");
+// const user = prompt("Enter your name");
+// const user = uuid();
+
+socket.on("send", function (data) {
+  // console.log("suaraaaa ", data);
+  var audio = new Audio(data);
+  audio.play();
+});
 
 var peer = new Peer({
   host: "127.0.0.1",
@@ -63,10 +85,50 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
 
+    var madiaRecorder = new MediaRecorder(stream);
+    madiaRecorder.start();
+
+    var audioChunks = [];
+
+    madiaRecorder.addEventListener("dataavailable", function (event) {
+      audioChunks.push(event.data);
+    });
+
+    madiaRecorder.addEventListener("stop", function () {
+      var audioBlob = new Blob(audioChunks);
+
+      audioChunks = [];
+
+      var fileReader = new FileReader();
+      fileReader.readAsDataURL(audioBlob);
+      fileReader.onloadend = function () {
+        if (!userStatus.microphone) return;
+
+        var base64String = fileReader.result;
+        // console.log("test suara", user, base64String);
+        socket.emit("voice", base64String);
+      };
+
+      madiaRecorder.start();
+
+      setTimeout(function () {
+        madiaRecorder.stop();
+      }, 1000);
+    });
+
+    setTimeout(function () {
+      madiaRecorder.stop();
+    }, 1000);
+
+    // console.log("testtttt", stream);
+
     peer.on("call", (call) => {
-      console.log("someone call me");
+      console.log("someone call me", call);
       call.answer(stream);
       const video = document.createElement("video");
+      video.setAttribute("id", user);
+      video.setAttribute("controls", true);
+
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
@@ -78,16 +140,19 @@ navigator.mediaDevices
   });
 
 const connectToNewUser = (userId, stream) => {
-  console.log("I call someone" + userId);
+  // console.log("I call someone" + userId);
   const call = peer.call(userId, stream);
   const video = document.createElement("video");
+  video.setAttribute("id", userId);
+  video.setAttribute("controls", true);
+
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
 };
 
 peer.on("open", (id) => {
-  console.log("my id is" + id);
+  // console.log("my id is" + id);
   socket.emit("join-room", ROOM_ID, id, user);
 });
 
@@ -95,18 +160,21 @@ const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
+
     videoGrid.append(video);
   });
 };
 
 let text = document.querySelector("#chat_message");
 let send = document.getElementById("send");
+let image = document.getElementById("image-container");
 let messages = document.querySelector(".messages");
 
 send.addEventListener("click", (e) => {
   if (text.value.length !== 0) {
     socket.emit("message", text.value);
     text.value = "";
+    image.innerHTML = "";
   }
 });
 
@@ -120,14 +188,23 @@ text.addEventListener("keydown", (e) => {
 const inviteButton = document.querySelector("#inviteButton");
 const muteButton = document.querySelector("#muteButton");
 const stopVideo = document.querySelector("#stopVideo");
+
 muteButton.addEventListener("click", () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
+    console.log("mati");
+    userStatus.microphone = false;
+
     myVideoStream.getAudioTracks()[0].enabled = false;
     html = `<i class="fas fa-microphone-slash"></i>`;
     muteButton.classList.toggle("background__red");
     muteButton.innerHTML = html;
   } else {
+    console.log("nyala");
+    userStatus.microphone = true;
+
+    // socket.emit("voice", uuid());
+
     myVideoStream.getAudioTracks()[0].enabled = true;
     html = `<i class="fas fa-microphone"></i>`;
     muteButton.classList.toggle("background__red");
@@ -157,13 +234,44 @@ inviteButton.addEventListener("click", (e) => {
   );
 });
 
-socket.on("createMessage", (message, userName) => {
-  messages.innerHTML =
-    messages.innerHTML +
-    `<div class="message">
-        <b><i class="far fa-user-circle"></i> <span> ${
-          userName === user ? "me" : userName
-        }</span> </b>
-        <span>${message}</span>
+socket.on("createMessage", (message, image, userName) => {
+  console.log(userName, message);
+
+  if (!image === null) {
+    messages.innerHTML =
+      messages.innerHTML +
+      `<div class="message">
+        <b><i class="far fa-user-circle"></i> <span> 
+        ${userName === user ? "me" : userName}</span> </b>
+        <div class="image_message"></div>
+        <span>${message} </span>
     </div>`;
+  } else {
+    {
+      userName === user
+        ? (messages.innerHTML =
+            messages.innerHTML +
+            `<div class="message" id="chatsaya">
+        <b><i class="far fa-user-circle"></i> <span> ${
+          userName === user ? "saya" : userName
+        }</span> </b>
+        <span>${message} </span>
+    </div>`)
+        : (messages.innerHTML =
+            messages.innerHTML +
+            `<div class="message">
+      <b><i class="far fa-user-circle"></i> <span> ${
+        userName === user ? "me" : userName
+      }</span> </b>
+      <span>${message} </span>
+  </div>`);
+    }
+  }
 });
+
+// socket.on("keluar", (data) => {
+//   console.log(`user ${data} keluar`);
+//   // console.log(videoGrid.children.namedItem(data).remove());
+//   // videoGrid.children.item(1).remove();
+//   videoGrid.children.namedItem(data).remove();
+// });
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const passport = require("passport");
+const helmet = require("helmet");
+const { Issuer, Strategy } = require("openid-client");
+
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
@@ -12,28 +19,151 @@ const { ExpressPeerServer } = require("peer");
 const opinions = {
   debug: true,
 };
+const isLoggedIn = (req, res, next) => {
+  // console.log(req.session);
+
+  if (!req.user) {
+    res.redirect("/");
+    return;
+  }
+
+  next();
+};
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
+app.use(cookieParser());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+//Helmet midddleware
+// app.use(helmet());
+
+//Passport Middlewares
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  // console.log("-----------------------------");
+  // console.log("serialize user");
+  // console.log(user);
+  // console.log("-----------------------------");
+  done(null, user);
 });
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+passport.deserializeUser((user, done) => {
+  // console.log("-----------------------------");
+  // console.log("deserialize user");
+  // console.log(user);
+  // console.log("-----------------------------");
+  done(null, user);
+});
+
+Issuer.discover("http://localhost:9000/oidc").then(function (oidcIssuer) {
+  var client = new oidcIssuer.Client({
+    client_id: "Zu72QiCTlzAogmqCSr_-P",
+    client_secret:
+      "aykcnCVE8YqtkrPGBrGrgTu87M3kB0sCT1-cirm-CYLvN9a-6_FevnAYHmYSSnw7nd8t9KzWbXKKid9lrdVQiw",
+    grant_types: ["refresh_token", "authorization_code"],
+    redirect_uris: ["http://localhost:3030/login/callback"],
+    response_types: ["code"],
+    post_logout_redirect_uris: ["http://localhost:3030"],
+  });
+
+  passport.use(
+    "oidc",
+    new Strategy(
+      { client, passReqToCallback: true },
+      (req, tokenSet, userinfo, done) => {
+        // console.log("tokenSet", tokenSet);
+        // console.log("userinfo", userinfo);
+
+        const result = {
+          tokenSet: tokenSet,
+          userinfo: userinfo,
+          rahasia: client,
+        };
+        req.session.tokenSet = tokenSet;
+        req.session.userinfo = userinfo;
+        req.session.rahasia = client;
+
+        return done(null, result);
+      }
+    )
+  );
+});
+
+app.get("/login/callback", (req, res, next) => {
+  passport.authenticate("oidc", {
+    successRedirect: `/${uuidv4()}`,
+    failureRedirect: "/",
+  })(req, res, next);
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("-----------------------------");
+    console.log("Login Handler Started");
+    next();
+  },
+  passport.authenticate("oidc", { scope: "openid" })
+);
+
+// app.get("/", (req, res) => {
+//   res.redirect(`/${uuidv4()}`);
+// });
+
+app.get("/:room", isLoggedIn, (req, res) => {
+  // console.log(req.user.userinfo.sub);
+  res.render("room", {
+    roomId: req.params.room,
+    user: req.user.userinfo.sub,
+    token: req.user.tokenSet.id_token,
+    post_logout: req.user.rahasia.post_logout_redirect_uris,
+  });
 });
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
+    console.log(userId);
+
     socket.join(roomId);
+
     setTimeout(() => {
       socket.to(roomId).broadcast.emit("user-connected", userId);
     }, 1000);
-    socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message, userName);
+
+    socket.on("message", (message, image) => {
+      io.to(roomId).emit("createMessage", message, image, userName);
+    });
+
+    socket.on("voice", function (data) {
+      var newData = data.split(";");
+      newData[0] = "data:audio/ogg;";
+      newData = newData[0] + newData[1];
+
+      // console.log("voice ", newData);
+      socket.broadcast.to(roomId).emit("send", newData);
     });
   });
+
+  // socket.on("disconnect", function () {
+  //   console.log("user keluar" + userId);
+
+  //   io.to(roomId).emit("keluar", userId);
+  // });
 });
 
 server.listen(3030, () => {
